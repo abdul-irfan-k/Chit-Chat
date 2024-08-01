bd1e3a5813b974e4720dbd2b956e12f476be0508
Irfan K <unknown7k7k@gmail.com>
2024-08-01 12:02:15 +0530

    updated

--- server/dist/util/random-text-generator.js
+++
@@ -1,7 +0,0 @@
-export const generateRandomUUID = (length) => {
-    const onlyLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return Array(length)
-        .fill(null)
-        .map(() => onlyLetters[Math.floor(Math.random() * onlyLetters.length)])
-        .join("");
-};
--- server/dist/util/socket.js
+++
@@ -1,3 +0,0 @@
-export const getSocketIp = (socket) => {
-    return socket.handshake.headers["x-real-ip"] || socket.request.connection.remoteAddress || "";
-};
